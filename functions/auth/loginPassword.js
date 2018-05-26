0e80a573da118e18e008e02da882de98abe9f07b
Gensen Choy <gensen.choy@gmail.com>
2018-05-25 23:32:32 -0700

    Don't load appstate initially

--- functions/auth/loginPassword.js
+++ functions/auth/loginPassword.js
@@ -7,18 +7,13 @@ const fs = require('fs');
  */
 module.exports = new Promise(function(resolve, reject) {
   try {
-    login({ appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8')) }, (err, api) => {
-      if (!err) return resolve(api);
-      console.error('Could not login from past session');
-      console.error(err);
-      login({ email: process.env.email, password: process.env.password }, (err, api) => {
-        if (err) {
-          reject(err);
-          return console.error(err);
-        }
-        fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
-        resolve(api);
-      });
+    login({ email: process.env.email, password: process.env.password }, (err, api) => {
+      if (err) {
+        reject(err);
+        return console.error(err);
+      }
+      fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+      resolve(api);
     });
   } catch (err) {
     reject(err);
