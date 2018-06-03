2db7cdfc1f66bde0767c21cc48e4591ff4dee97d
Gensen Choy <gensen.choy@gmail.com>
2018-06-03 00:35:33 -0700

    Set user agent

--- functions/auth/loginPassword.js
+++ functions/auth/loginPassword.js
@@ -7,7 +7,7 @@ const fs = require('fs');
  */
 module.exports = new Promise(function(resolve, reject) {
   try {
-    login({ email: process.env.email, password: process.env.password }, (err, api) => {
+    login({ email: process.env.email, password: process.env.password, userAgent: process.env.userAgent }, (err, api) => {
       if (err) {
         reject(err);
         return console.error(err);
